20069720326a8cff8918fe0ae29a890136dc16f4
EREMUS Marc <marceremus@gmail.com>
2020-12-15 13:53:21 +0100

    Create bases.py

---
+++ bases.py
@@ -0,0 +1,30 @@
+
+def main():
+
+    portefeuil = 5000
+    pc_price = 800
+    if(portefeuil > pc_price):
+        print("Vous pouvez acheter votre ordinateur")
+    if else:
+
+    # note_1 = input("Entrez la première note")
+    # print("Votre note est : " + note_1)
+    # note_2 = input("Entrez la première note")
+    # print("Votre deuxième note est : " + note_1)
+    # print(avg(int(note_1), int(note_2)))
+    # création d'un variable
+    # username = "Marc"
+    # age = 19
+    # prix = 200.18
+    # is_happy = True
+    # print(username + ", a  " + str(age) + " ans")
+
+    # hello = " Hello World"
+    # print(hello)
+
+def avg(n,m):
+    return (n+m)/2
+
+
+if __name__ == '__main__':
+    main()
